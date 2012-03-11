a6216bbad5f507bdbb2562dd3cc2e5b31ef89264
Fabian Lenz <lenz_fabian@yahoo.de>
2012-03-12 00:58:12 +0100

    + Lots of stuff...

--- js/index.js
+++ js/index.js
@@ -1,56 +1,83 @@
-var streamViewModel = new StreamViewModel();
+var streamViewModel = new StreamViewModel(true);
 
 $(document).ready(function() {
-    var url = "http://api.justin.tv/api/stream/list.json?category=gaming&strategy&meta_game=Heroes%20of%20Newerth&limit=10";
-    $.getJSON(url + "&jsonp=?", function(data) {
-        $.each(data, function(index, value) {
-            var stream = new Stream();
-            stream.views = value.channel_count;
-            stream.user = value.channel.title;
-            stream.embedCode = value.channel.embed_code;
-            streamViewModel.addStream(stream);
-        });
-    });
-
-    ko.applyBindings(streamViewModel);
-
     $("#streamList").on("click", function(event) {
         var stream = ko.dataFor(event.target);
 
         // TODO: Find better solution for this crap
         var embedString = stream.embedCode.replace("auto_play=false", "auto_play=true");
         embedString = embedString.replace("height=\"295\" width=\"353\"", "height=\"500\" width=\"800\"");
-        $("#playerWindow").html(embedString);
+        $("#embed").html(embedString);
     });
+
+    $("#playerWindow").resizable({
+        minHeight:150,
+        minWidth:200,
+        handles: "ne, se, sw, nw",
+        stop: function (event, ui) {
+            $.jStorage.set("playerWidth", ui.size.width);
+            $.jStorage.set("playerHeight", ui.size.height);
+        }
+    });
+    var width = $.jStorage.get("playerWidth", $("#playerWindow").width());
+    var height = $.jStorage.get("playerHeight", $("#playerWindow").height());
+
+    $("#playerWindow").width(width);
+    $("#playerWindow").height(height);
 
     if (!FlashDetect.installed) {
         $("#playerWindow").html("<h1>Please enable Flash to watch streams!</h1>");
     }
-});
 
-function fakeData(data)
-{
-    $.each(data, function (index, value) {
-        var stream = new Stream();
-        stream.views = value.channel_count;
-        stream.user = value.channel.title;
-        stream.embedCode = value.channel.embed_code;
-        streamViewModel.addStream(stream);
-    });
-}
+    ko.applyBindings(streamViewModel);
+});
 
 function Stream() {
     this.views = 0;
     this.title = "default";
     this.embedCode = "";
+    this.screenCapUrl = "";
 }
 
-function StreamViewModel() {
+function StreamViewModel(autoRefresh) {
     var self = this;
 
-    this.streams = ko.observableArray([]);
+    this.streams = ko.observableArray();
+    this.secondsToRefresh = ko.observable(60);
 
-    self.addStream = function (stream) {
+    self.addStream = function(stream) {
         self.streams.push(stream);
     }
+
+    self.fromArray = function(array) {
+        self.streams.removeAll();
+        $.each(array, function (index, value) {
+            var stream = new Stream();
+            stream.views = value.channel_count;
+            stream.user = value.channel.title;
+            stream.embedCode = value.channel.embed_code;
+            stream.screenCapUrl = value.channel.screen_cap_url_small;
+            self.addStream(stream);
+        });
+    }
+
+    self.update = function() {
+        var seconds = self.secondsToRefresh() - 1;
+        self.secondsToRefresh(seconds);
+        if (seconds <= 0) {
+            self.fetchStreams();
+            self.secondsToRefresh(60);
+        }
+    }
+
+    self.fetchStreams = function() {
+        var url = "http://api.justin.tv/api/stream/list.json?category=gaming&strategy&meta_game=Heroes%20of%20Newerth&limit=10";
+        $.getJSON(url + "&jsonp=?", function (data) {
+            self.fromArray(data);
+        });
+    }
+
+    self.fetchStreams();
+
+    if (autoRefresh) setInterval(self.update, 1000);
 }
